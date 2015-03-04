f730c6d2836fd69d73df3b78ed7d5d94831fec65
Aleksey Shipilev <shade@openjdk.org>
2015-03-04 13:40:34 +0300

    Update the wording in JOLSample_06_Gaps.java

--- jol-samples/src/main/java/org/openjdk/jol/samples/JOLSample_06_Gaps.java
+++ jol-samples/src/main/java/org/openjdk/jol/samples/JOLSample_06_Gaps.java
@@ -43,9 +43,8 @@ public class JOLSample_06_Gaps {
     /*
      * This example shows another HotSpot layout quirk.
      *
-     * HotSpot rounds up the instance field block to four bytes.
-     * That unfortunately yields the artificial gaps at the end of
-     * the class.
+     * HotSpot rounds up the instance field block up to reference size.
+     * That unfortunately yields the artificial gaps at the end of the class.
      *
      * See also:
      *  https://bugs.openjdk.java.net/browse/JDK-8024912
