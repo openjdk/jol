23c9c76e0e199407a37e822fa3c36c0e3981b0ee
Aleksey Shipilev <shade@openjdk.org>
2020-04-14 09:51:33 +0200

    JOLSample_26_IHC_BL_Conflict example

---
+++ jol-samples/src/main/java/org/openjdk/jol/samples/JOLSample_26_IHC_BL_Conflict.java
@@ -0,0 +1,96 @@
+/*
+ * Copyright (c) 2020, Red Hat, Inc.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *  * Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ *
+ *  * Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ *  * Neither the name of Oracle nor the names of its contributors may be used
+ *    to endorse or promote products derived from this software without
+ *    specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
+ * THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package org.openjdk.jol.samples;
+
+import org.openjdk.jol.info.ClassLayout;
+import org.openjdk.jol.vm.VM;
+
+import java.util.concurrent.TimeUnit;
+
+import static java.lang.System.out;
+
+/**
+ * @author Aleksey Shipilev
+ */
+public class JOLSample_26_IHC_BL_Conflict {
+
+    /*
+     * This is the example of biased locking.
+     *
+     * In order to demonstrate this, we first need to sleep for >5 seconds
+     * to pass the grace period of biased locking. Then, we do the same
+     * trick as the example before. You may notice that the mark word
+     * had not changed after the lock was released. That is because
+     * the mark word now contains the reference to the thread this object
+     * was biased to.
+     */
+
+    public static void main(String[] args) throws Exception {
+        out.println(VM.current().details());
+
+        TimeUnit.SECONDS.sleep(6);
+
+        final A a = new A();
+
+        ClassLayout layout = ClassLayout.parseInstance(a);
+
+        out.println("**** Fresh object");
+        out.println(layout.toPrintable());
+
+        synchronized (a) {
+            out.println("**** With the lock");
+            out.println(layout.toPrintable());
+        }
+
+        out.println("**** After the lock");
+        out.println(layout.toPrintable());
+
+        int hashCode = a.hashCode();
+        out.println("hashCode: " + Integer.toHexString(hashCode));
+        out.println();
+
+        out.println("**** After the hashcode");
+        out.println(layout.toPrintable());
+
+        synchronized (a) {
+            out.println("**** With the second lock");
+            out.println(layout.toPrintable());
+        }
+
+        out.println("**** After the second lock");
+        out.println(layout.toPrintable());
+    }
+
+    public static class A {
+        // no fields
+    }
+
+}
