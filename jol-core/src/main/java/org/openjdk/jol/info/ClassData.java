bfc726154bd0452ff7535c8bb91bf2601f905b57
Sergey Ponomarev <stokito@gmail.com>
2020-09-30 15:30:13 +0300

    ClassData: Return List instead of Collection
    This makes working easier because we can call get(idx).
    Added ClassDataTest
    
    Signed-off-by: Sergey Ponomarev <stokito@gmail.com>

--- jol-core/src/main/java/org/openjdk/jol/info/ClassData.java
+++ jol-core/src/main/java/org/openjdk/jol/info/ClassData.java
@@ -28,7 +28,6 @@ import java.lang.ref.WeakReference;
 import java.lang.reflect.Field;
 import java.lang.reflect.Modifier;
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 
@@ -226,7 +225,7 @@ public class ClassData {
      *
      * @return field data
      */
-    public Collection<FieldData> fields() {
+    public List<FieldData> fields() {
         if (isArray) {
             return Collections.emptyList();
         } else {
@@ -239,7 +238,7 @@ public class ClassData {
      *
      * @return field data
      */
-    public Collection<FieldData> ownFields() {
+    public List<FieldData> ownFields() {
         return fieldsFor(classNames.get(classNames.size() - 1));
     }
 
@@ -277,7 +276,7 @@ public class ClassData {
      * @param klass class name
      * @return field data
      */
-    public Collection<FieldData> fieldsFor(String klass) {
+    public List<FieldData> fieldsFor(String klass) {
         List<FieldData> r = new ArrayList<>();
         for (FieldData f : fields) {
             if (f.hostClass().equals(klass)) {
