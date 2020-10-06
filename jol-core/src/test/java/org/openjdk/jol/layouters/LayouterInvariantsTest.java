4e4f79321ed9fb6b135f2e8dcbd37b15786d8d10
Aleksey Shipilev <shade@redhat.com>
2020-10-06 11:49:30 +0200

    Optimize LayouterInvariantsTest

--- jol-core/src/test/java/org/openjdk/jol/layouters/LayouterInvariantsTest.java
+++ jol-core/src/test/java/org/openjdk/jol/layouters/LayouterInvariantsTest.java
@@ -1,6 +1,7 @@
 package org.openjdk.jol.layouters;
 
 import junit.framework.Assert;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.openjdk.jol.datamodel.CurrentDataModel;
 import org.openjdk.jol.datamodel.*;
@@ -12,61 +13,64 @@ import java.util.Random;
 
 public class LayouterInvariantsTest {
 
-    private static final boolean[] BOOLS = {false, true};
     private static final DataModel[] MODELS = {
         new CurrentDataModel(),
         new X86_32_DataModel(),
         new X86_64_COOPS_DataModel(),
         new X86_64_DataModel()
     };
-    private static final int ITERATIONS = 20000;
+
+    private static final int ITERATIONS = 10000;
+
+    private static Class<?>[] CLS;
+    private static int[] SEEDS;
+
+    @BeforeClass
+    public static void buildClasses() throws Exception {
+        Random seeder = new Random();
+        CLS = new Class<?>[ITERATIONS];
+        SEEDS = new int[ITERATIONS];
+        for (int c = 0; c < ITERATIONS; c++) {
+            SEEDS[c] = seeder.nextInt();
+            CLS[c] = ClassGenerator.generate(new Random(SEEDS[c]), 5, 50);
+        }
+    }
 
     @Test
-    public void testRaw() throws Exception {
-        Random seeder = new Random();
+    public void testRaw() {
         for (int c = 0; c < ITERATIONS; c++) {
-            int seed = seeder.nextInt();
-            Class<?> cl = ClassGenerator.generate(new Random(seed), 5, 50);
-
             for (DataModel model : MODELS) {
                 try {
-                    ClassLayout.parseClass(cl, new RawLayouter(model));
+                    ClassLayout.parseClass(CLS[c], new RawLayouter(model));
                 } catch (Exception e) {
-                    Assert.fail("Failed. Seed = " + seed);
+                    Assert.fail("Failed. Seed = " + SEEDS[c]);
                 }
             }
         }
     }
 
     @Test
-    public void testCurrent() throws Exception {
-        Random seeder = new Random();
+    public void testCurrent() {
         for (int c = 0; c < ITERATIONS; c++) {
-            int seed = seeder.nextInt();
-            Class<?> cl = ClassGenerator.generate(new Random(seed), 5, 50);
-
             try {
-                ClassLayout.parseClass(cl, new CurrentLayouter());
+                ClassLayout.parseClass(CLS[c], new CurrentLayouter());
             } catch (Exception e) {
-                Assert.fail("Failed. Seed = " + seed);
+                Assert.fail("Failed. Seed = " + SEEDS[c]);
             }
         }
     }
 
     @Test
-    public void testHotspot() throws Exception {
-        Random seeder = new Random();
+    public void testHotspot() {
         for (int c = 0; c < ITERATIONS; c++) {
-            int seed = seeder.nextInt();
-            Class<?> cl = ClassGenerator.generate(new Random(seed), 5, 50);
-            ClassData cd = ClassData.parseClass(cl);
+            ClassData cd = ClassData.parseClass(CLS[c]);
             try {
                 for (DataModel model : MODELS) {
                     HotSpotLayouter layouter = new HotSpotLayouter(model);
                     layouter.layout(cd);
                 }
             } catch (Exception e) {
-                Assert.fail("Failed. Seed = " + seed);
+                Assert.fail("Failed. Seed = " + SEEDS[c]);
             }
         }
     }
