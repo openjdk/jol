d0e6ff6be16b6ac5a7d527107f2958bcaffda725
Aleksey Shipilev <shipilev@amazon.de>
2023-06-26 15:29:54 +0200

    Fix

--- jol-core/src/test/java/org/openjdk/jol/util/MathUtilTest.java
+++ jol-core/src/test/java/org/openjdk/jol/util/MathUtilTest.java
@@ -3,6 +3,8 @@ package org.openjdk.jol.util;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.text.NumberFormat;
+
 public class MathUtilTest {
 
     static int slowIntAlign(int v, int a) {
@@ -55,6 +57,51 @@ public class MathUtilTest {
                 Assert.assertEquals(slowLongAlign(v, a), MathUtil.align(v, a));
             }
         }
+    }
+
+    @Test
+    public void inProperUnitsTest() {
+        Assert.assertEquals("0", MathUtil.inProperUnits(0));
+        Assert.assertEquals("1", MathUtil.inProperUnits(1));
+        Assert.assertEquals("10", MathUtil.inProperUnits(10));
+        Assert.assertEquals("100", MathUtil.inProperUnits(100));
+        Assert.assertEquals("1000", MathUtil.inProperUnits(1000));
+        Assert.assertEquals("10000", MathUtil.inProperUnits(10_000));
+        Assert.assertEquals("100000", MathUtil.inProperUnits(100_000));
+
+        Assert.assertEquals("100K", MathUtil.inProperUnits(100_000 + 1));
+        Assert.assertEquals("1000K", MathUtil.inProperUnits(1_000_000));
+        Assert.assertEquals("10000K", MathUtil.inProperUnits(10_000_000));
+        Assert.assertEquals("100000K", MathUtil.inProperUnits(100_000_000));
+
+        Assert.assertEquals("100M", MathUtil.inProperUnits(100_000_000 + 1));
+        Assert.assertEquals("1000M", MathUtil.inProperUnits(1_000_000_000));
+        Assert.assertEquals("10000M", MathUtil.inProperUnits(10_000_000_000L));
+        Assert.assertEquals("100000M", MathUtil.inProperUnits(100_000_000_000L));
+
+        Assert.assertEquals("100G", MathUtil.inProperUnits(100_000_000_000L + 1));
+        Assert.assertEquals("1000G", MathUtil.inProperUnits(1_000_000_000_000L));
+        Assert.assertEquals("10000G", MathUtil.inProperUnits(10_000_000_000_000L));
+        Assert.assertEquals("100000G", MathUtil.inProperUnits(100_000_000_000_000L));
+    }
+
+    @Test
+    public void diffInPercentTest() {
+        Assert.assertEquals("0%", MathUtil.diffPercent(0, 0));
+        Assert.assertEquals("0%", MathUtil.diffPercent(1, 1));
+
+        Assert.assertEquals("~0%",   MathUtil.diffPercent(10001, 10000));
+        Assert.assertEquals("~0%",   MathUtil.diffPercent(10009, 10000));
+
+        NumberFormat nf = NumberFormat.getInstance();
+        nf.setMinimumFractionDigits(1);
+        nf.setMaximumFractionDigits(1);
+
+        Assert.assertEquals("+" + nf.format(0.1) + "%", MathUtil.diffPercent(10011, 10000));
+        Assert.assertEquals("-" + nf.format(0.1) + "%", MathUtil.diffPercent( 9989, 10000));
+
+        Assert.assertEquals("+" + nf.format(5) + "%", MathUtil.diffPercent(1050, 1000));
+        Assert.assertEquals("-" + nf.format(5) + "%", MathUtil.diffPercent( 950, 1000));
     }
 
 }
