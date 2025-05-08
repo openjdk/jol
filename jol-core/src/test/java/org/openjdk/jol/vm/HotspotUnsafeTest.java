95ad59a1fe00a4ba60cfe5e1cd04acb5776b47d0
raipc <anton-rib@mail.ru>
2025-05-08 02:19:11 +0200

    Added test for object alignment detection

---
+++ jol-core/src/test/java/org/openjdk/jol/vm/HotspotUnsafeTest.java
@@ -0,0 +1,25 @@
+package org.openjdk.jol.vm;
+
+import org.junit.Test;
+
+import java.lang.management.ManagementFactory;
+import java.util.Optional;
+
+import static org.apache.commons.lang3.StringUtils.substringAfter;
+import static org.junit.Assert.assertEquals;
+
+public class HotspotUnsafeTest {
+    @Test
+    public void testAlignment(){
+        assertEquals(expectedAlignment(), VM.current().objectAlignment());
+    }
+
+    private int expectedAlignment() {
+        Optional<String> alignmentProperty = ManagementFactory.getRuntimeMXBean().getInputArguments().stream()
+                .filter(arg -> arg.contains("-XX:ObjectAlignmentInBytes"))
+                .findAny();
+        return alignmentProperty
+                .map(alignment -> Integer.parseInt(substringAfter(alignment, "=")))
+                .orElseGet(() -> Integer.parseInt(System.getProperty("sun.arch.data.model")) / Byte.SIZE);
+    }
+}
