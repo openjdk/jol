87b149692042f53e651e76d413a56d83e0da4c91
Anton Rybochkin <arybochkin@yandex-team.ru>
2025-06-06 18:04:28 +0200

    Update jol-core/src/main/java/org/openjdk/jol/vm/ZGCAddress.java
    Co-authored-by: Aleksey Shipilëv <shipilev@amazon.de>

--- jol-core/src/main/java/org/openjdk/jol/vm/ZGCAddress.java
+++ jol-core/src/main/java/org/openjdk/jol/vm/ZGCAddress.java
@@ -9,7 +9,7 @@ class ZGCAddress {
     private static final long REMAPPED_BITS_MASK = 0b1111L << 12;
     private static final long CLEAR_UNUSED_BITS_MASK = (1L << 46) - 1;
     private static final long COLOR_BITS_COUNT = 16;
-    private static final boolean isAarch = "aarch64".equals(System.getProperty("os.arch"));
+    private static final boolean IS_ARM64 = "aarch64".equals(System.getProperty("os.arch"));
 
     static long uncolorize(long address) {
         return isAarch ? uncolorizeAarch(address) : uncolorizeNonAarch(address);
